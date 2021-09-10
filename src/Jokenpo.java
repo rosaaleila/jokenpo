bcf1d2581b48bc07d680a71785736281dfd17cce
LEILA ROSA PEREIRA <21276364@CFP127ALUNO.LOCAL>
2021-09-10 16:19:28 -0300

    codigo funcionando

--- src/Jokenpo.java
+++ src/Jokenpo.java
@@ -7,24 +7,33 @@ public class Jokenpo {
 		
 		Scanner leitor = new Scanner(System.in);
 		Random gerador = new Random();
-	
-		int partidas = 0;
-		int contador = 1;
+		
+		String decisao = "S";
 		int vitoriasJog = 0;			
 		int vitoriasPC = 0;
-		String decisao = "S";
-			
+		
 		while (decisao.equals("s") || decisao.equals("S")) {
+			
+			vitoriasJog = 0;			
+			vitoriasPC = 0;
+			int escolhaJog = 0;
+			int escolhaPC = 0;
+			String jogadaJog;
+			String jogadaPC;
+			int partidas = 0;
+			int contador = 1;
+			int empates = 0;
+		
 			System.out.println("Bem-vindo ao jogo jokenpo!");
 			System.out.println();
 				
-			System.out.print("Digite quantas partidas você deseja que a brincadeira tenha: ");
+			System.out.print("Digite quantas partidas você deseja que o jogo tenha: ");
 			partidas = leitor.nextInt();
 				
-			if (partidas % 2 == 0 || partidas < 3) {
+			while (partidas % 2 == 0 || partidas < 3) {
 			System.out.println("Por favor, tente novamente e digite um número ímpar e/ou maior e/ou igual a 3!!!");
 			System.out.println();
-			System.out.print("Digite quantas partidas você deseja que a brincadeira tenha: ");
+			System.out.print("Digite novamente quantas partidas você deseja que o jogo tenha: ");
 			partidas = leitor.nextInt();
 			} 
 				
@@ -36,21 +45,22 @@ public class Jokenpo {
 			System.out.println("3 - Tesoura");
 			System.out.println();
 				
-			while (contador <= partidas) {	
-				vitoriasJog = 0;			
-				vitoriasPC = 0;
-				int escolhaJog = 0;
-				int escolhaPC = 0;
-				String jogadaJog;
-				String jogadaPC;
+			while (contador <= partidas) {
 					
 				System.out.println("Partida " + contador);
 				System.out.println();
 					
 				System.out.print("Digite sua escolha: ");
 				escolhaJog = leitor.nextInt();
-					
+				
 				escolhaPC = gerador.nextInt(3) + 1;
+				
+				while (escolhaJog < 1 || escolhaJog > 3) {
+					System.out.println("Por favor, escolha um valor válido.");
+					System.out.print("Digite novamente sua escolha: ");
+					escolhaJog = leitor.nextInt();
+					System.out.println();
+				}
 					
 				if (escolhaJog == 1) {
 					jogadaJog = "Pedra";
@@ -68,79 +78,97 @@ public class Jokenpo {
 					jogadaPC = "Tesoura";
 				}
 					
+				if (escolhaPC == 3 && escolhaJog == 2 || escolhaPC == 2 && escolhaJog == 1 || escolhaPC == 1 && escolhaJog == 3) {
+					System.out.println("Você escolheu " + jogadaJog);
+					System.out.println("O computador escolheu " + jogadaPC);
+					System.out.println("Computador venceu!");
+					System.out.println();
+					vitoriasPC++;
+				} else if (escolhaJog == 3 && escolhaPC == 2 || escolhaJog == 2 && escolhaPC == 1 || escolhaJog == 1 && escolhaPC == 3) {
+					System.out.println("Você escolheu " + jogadaJog);
+					System.out.println("O computador escolheu " + jogadaPC);
+					System.out.println("Você venceu!");
+					System.out.println();
+					vitoriasJog++;
+				} else {
+					System.out.println("Houve um empate!"); 
+					System.out.println();
+					empates++;
+				}
 					
-				if (escolhaPC == 3) {
-					if (escolhaJog == 2) {
-						System.out.println("Você escolheu " + jogadaJog);
-						System.out.println("O computador escolheu " + jogadaPC);
-						System.out.println("Computador venceu!");
-						System.out.println();
-						vitoriasPC++;
-					} else if (escolhaJog == 1) {
-						System.out.println("Você escolheu " + jogadaJog);
-						System.out.println("O computador escolheu " + jogadaPC);
-						System.out.println("Você venceu!");
-						System.out.println();
-						vitoriasJog++;
-					} else {
-						System.out.println("Houve um empate!");
-						vitoriasJog++;
-						vitoriasPC++;
-					}
-				} else if (escolhaPC == 2) {
-					if (escolhaJog == 3) {
-						System.out.println("Você escolheu " + jogadaJog);
-						System.out.println("O computador escolheu " + jogadaPC);
-						System.out.println("Você venceu!");
-						System.out.println();
-						vitoriasJog++;
-					} else if (escolhaJog == 1) {
-						System.out.println("Você escolheu " + jogadaJog);
-						System.out.println("O computador escolheu " + jogadaPC);
-						System.out.println("Computador venceu!");
-						System.out.println();
-						vitoriasPC++;
-					} else {
-						System.out.println("Houve um empate!");
-					}
-				} else if (escolhaPC == 1) {
-					if (escolhaJog == 3) {
-						System.out.println("Você escolheu " + jogadaJog);
-						System.out.println("O computador escolheu " + jogadaPC);
-						System.out.println("Computador venceu!");
-						System.out.println();
-						vitoriasPC++;
-					} else if (escolhaJog == 2) {
-						System.out.println("Você escolheu " + jogadaJog);
-						System.out.println("O computador escolheu " + jogadaPC);
-						System.out.println("Você venceu!");
-						System.out.println();
-						vitoriasJog++;
-					} else {
-						System.out.println("Houve um empate!");
-					}
+				if (vitoriasPC >= partidas / 2 + 1 || vitoriasJog >= partidas / 2 + 1) {
+					contador = partidas;
 				}
 				
-				contador++;
+				if (vitoriasPC == vitoriasJog) {
+						empates++;
+				}
 				
-				
-				if (vitoriasPC > vitoriasJog) {
-					System.out.printf("Computador venceu por %d x %d", vitoriasPC, vitoriasJog);
+				if (contador == partidas) {
+					while (empates == (partidas / 2) || vitoriasPC == vitoriasJog) {
+					System.out.println("Partida bônus!!");
 					System.out.println();
+						
+					System.out.print("Digite sua escolha: ");
+					escolhaJog = leitor.nextInt();
+						
+					while (escolhaJog < 1 || escolhaJog > 3) {
+						System.out.println("Por favor, escolha um valor válido.");
+						System.out.print("Digite novamente sua escolha: ");
+						escolhaJog = leitor.nextInt();
+						System.out.println(); 
+					}
+						
+					if (escolhaPC == 3 && escolhaJog == 2 || escolhaPC == 2 && escolhaJog == 1 || escolhaPC == 1 && escolhaJog == 3) {
+						System.out.println("Você escolheu " + jogadaJog);
+						System.out.println("O computador escolheu " + jogadaPC);
+						System.out.println("Computador venceu!");
+						System.out.println();
+						vitoriasPC++;
+					} else if (escolhaJog == 3 && escolhaPC == 2 || escolhaJog == 2 && escolhaPC == 1 || escolhaJog == 1 && escolhaPC == 3) {
+						System.out.println("Você escolheu " + jogadaJog);
+						System.out.println("O computador escolheu " + jogadaPC);
+						System.out.println("Você venceu!");
+						System.out.println();
+						vitoriasJog++;
 					} else {
-					System.out.printf("Você venceu por %d x %d", vitoriasJog, vitoriasPC);
-					System.out.println();
+						System.out.println("Houve um empate!"); 
+						System.out.println();
+						empates++;
 					}
 					
-					System.out.print("Deseja jogar mais vezes? Digite S para SIM ou N para NÃO: ");
-					decisao = leitor.next();
+					
+					} //while
 				} 
-				
+						contador++;
 			}
-		
-		
-		leitor.close();
-		
-	}
-
+			
+			if (vitoriasPC > vitoriasJog) {
+				System.out.printf("Computador venceu por %d x %d", vitoriasPC, vitoriasJog);
+				System.out.println();
+			} else {
+				System.out.printf("Você venceu por %d x %d", vitoriasJog, vitoriasPC);
+				System.out.println();
+			}
+			
+			System.out.println("Deseja jogar novamente? Digite S para SIM e N para NÃO: ");
+			decisao = leitor.next();
+			
+			if (decisao.equals("S") || decisao.equals("s")) {
+				System.out.println("Vamos lá!");
+				System.out.println();
+			} else if (decisao.equals("N") || decisao.equals("n")) {
+				System.out.println("Te vejo na próxima!");
+				System.out.println("Fim.");
+			} else {
+				System.out.println("Por favor, digite uma opção válida.");
+				System.out.println("Deseja jogar novamente? Digite S para SIM e N para NÃO: ");
+				decisao = leitor.next();
+			}
+				
+			} //while1
+				
+				leitor.close();
+				
+				}
 }
