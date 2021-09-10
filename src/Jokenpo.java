8024dfcb239b33178c434193eba8429ae1be6829
LEILA ROSA PEREIRA <21276364@CFP127ALUNO.LOCAL>
2021-09-10 16:46:19 -0300

    adicionando comentarios

--- src/Jokenpo.java
+++ src/Jokenpo.java
@@ -12,7 +12,11 @@ public class Jokenpo {
 		int vitoriasJog = 0;			
 		int vitoriasPC = 0;
 		
+		// criando laço principal
+		
 		while (decisao.equals("s") || decisao.equals("S")) {
+			
+			//zerando variaveis
 			
 			vitoriasJog = 0;			
 			vitoriasPC = 0;
@@ -30,6 +34,8 @@ public class Jokenpo {
 			System.out.print("Digite quantas partidas você deseja que o jogo tenha: ");
 			partidas = leitor.nextInt();
 				
+			//verificando se o num de partidas é valido
+			
 			while (partidas % 2 == 0 || partidas < 3) {
 			System.out.println("Por favor, tente novamente e digite um número ímpar e/ou maior e/ou igual a 3!!!");
 			System.out.println();
@@ -44,6 +50,8 @@ public class Jokenpo {
 			System.out.println("2 - Papel");
 			System.out.println("3 - Tesoura");
 			System.out.println();
+			
+			// laco de repeticao das partidas
 				
 			while (contador <= partidas) {
 					
@@ -55,6 +63,8 @@ public class Jokenpo {
 				
 				escolhaPC = gerador.nextInt(3) + 1;
 				
+				//verificando se a escolha do usuario é valida
+				
 				while (escolhaJog < 1 || escolhaJog > 3) {
 					System.out.println("Por favor, escolha um valor válido.");
 					System.out.print("Digite novamente sua escolha: ");
@@ -62,6 +72,8 @@ public class Jokenpo {
 					System.out.println();
 				}
 					
+				//anotando escolha do jogador/pc em string
+				
 				if (escolhaJog == 1) {
 					jogadaJog = "Pedra";
 				} else if (escolhaJog == 2) {
@@ -78,6 +90,8 @@ public class Jokenpo {
 					jogadaPC = "Tesoura";
 				}
 					
+				//verificando vencedores e empates
+				
 				if (escolhaPC == 3 && escolhaJog == 2 || escolhaPC == 2 && escolhaJog == 1 || escolhaPC == 1 && escolhaJog == 3) {
 					System.out.println("Você escolheu " + jogadaJog);
 					System.out.println("O computador escolheu " + jogadaPC);
@@ -96,13 +110,13 @@ public class Jokenpo {
 					empates++;
 				}
 					
+				//finalizando contador caso as vitorias sejam suficientes para nao podermos desempatar
+				
 				if (vitoriasPC >= partidas / 2 + 1 || vitoriasJog >= partidas / 2 + 1) {
 					contador = partidas;
 				}
 				
-				if (vitoriasPC == vitoriasJog) {
-						empates++;
-				}
+				// criando rodadas extras
 				
 				if (contador == partidas) {
 					while (empates == (partidas / 2) || vitoriasPC == vitoriasJog) {
@@ -137,11 +151,14 @@ public class Jokenpo {
 						empates++;
 					}
 					
+					//fim das rodadas
 					
-					} //while
+					} 
 				} 
 						contador++;
-			}
+			} //while2
+			
+			//verificando vencedor
 			
 			if (vitoriasPC > vitoriasJog) {
 				System.out.printf("Computador venceu por %d x %d", vitoriasPC, vitoriasJog);
@@ -150,6 +167,8 @@ public class Jokenpo {
 				System.out.printf("Você venceu por %d x %d", vitoriasJog, vitoriasPC);
 				System.out.println();
 			}
+			
+			//verificando escolha
 			
 			System.out.println("Deseja jogar novamente? Digite S para SIM e N para NÃO: ");
 			decisao = leitor.next();
@@ -166,7 +185,7 @@ public class Jokenpo {
 				decisao = leitor.next();
 			}
 				
-			} //while1
+			} // while1 
 				
 				leitor.close();
 				
